8bb23a06aa8bb3e6d0bb2e40341207a36cd42094
phuoc <phuocdongxoai@gmail.com>
2025-04-08 21:17:26 +0700

    wip server basic

---
+++ basic/server.rs
@@ -0,0 +1,93 @@
+use rustls::{
+  pki_types::{CertificateDer, PrivateKeyDer},
+  crypto::{CryptoProvider, aws_lc_rs},
+};
+use std::{
+  fs, 
+  net::SocketAddr, 
+  path::Path, 
+  sync::Arc
+};
+use anyhow::{Context, Result, bail};
+use quinn::{
+  crypto::rustls::QuicServerConfig,
+  Endpoint,
+  ServerConfig,
+  SendStream, 
+  RecvStream,
+};
+use colored::*;
+// use  proto::crypto::rustls::QuicServerConfig,
+
+const CERT_DIR: &str = "/tmp/quinn_certs";
+
+#[tokio::main]
+async fn main() -> Result<()> {
+  let cert_dir: &Path = Path::new(CERT_DIR);
+  let cert_path= cert_dir.join("cert.der");
+  let key_path = cert_dir.join("key.der");
+
+  let bytes = fs::read(cert_path).context("failed to read certificate")?;
+  let cert = CertificateDer::try_from(bytes)?;
+
+  let bytes = fs::read(key_path).context("failed to read private key")?;
+  let key =  PrivateKeyDer::try_from(bytes).map_err(anyhow::Error::msg)?;
+
+  CryptoProvider::install_default(
+    aws_lc_rs::default_provider()
+  ).expect("failed to install default crypto provider");
+
+  let tls_config = rustls::ServerConfig::builder()
+  .with_no_client_auth()
+  .with_single_cert(vec![cert], key)?;
+
+  let quic_config = QuicServerConfig::try_from(tls_config)?;
+  let server_config = ServerConfig::with_crypto(Arc::new(quic_config));
+
+  let addr: SocketAddr = "127.0.0.1:4843".parse()?;
+  let endpoint = Endpoint::server(server_config, addr)?;
+
+  let addr = endpoint.local_addr()?;
+  println!(
+    "{} {}",
+    "🚀 QUIC server listening at:".bold().green(),
+    addr.to_string().blue());
+
+  while let Some(conn) = endpoint.accept().await {
+    println!("accepting connection from {}", conn.remote_address())
+
+  }
+  Ok(())
+}
+
+async fn handle_conn(incomming: quinn::Incoming) -> Result<()> {
+  let conn = incomming.await?;
+  println!("established connection from {}", conn.remote_address());
+  loop {
+    let stream = conn.accept_bi().await;
+    let stream = match stream {
+      Err(quinn::ConnectionError::ApplicationClosed { .. }) => {
+        println!("connection closed");
+        return Ok(());
+    }
+      Err(e) => {
+        return Err(e.into());
+      }
+      Ok(s) => s
+    };
+    tokio::spawn(async move {
+      handle_stream(stream).await
+    });
+  }
+}
+
+async fn handle_stream((mut send, mut recv):(SendStream, RecvStream)) -> Result<()> {
+  let req = recv
+  .read_to_end(64*1024)
+  .await
+  .context("failed reading request");
+
+
+
+  Ok(())
+}
